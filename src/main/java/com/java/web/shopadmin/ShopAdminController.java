007265262099bf9f699aa253f916b8c2213603e6
zhumengstar <1378484330@qq.com>
2018-11-13 20:54:45 +0800

    商品添加web

--- src/main/java/com/java/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/java/web/shopadmin/ShopAdminController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
+ *
+ * 解析路由并转发
  * @author:zhumeng
  * @desc:
  **/
@@ -14,33 +16,38 @@ public class ShopAdminController {
 
     @RequestMapping(value = "/shopoperation")
     public String shopOperation() {
+        //转发至店铺注册或者编辑界面
         return "shop/shopoperation";
     }
 
     @RequestMapping(value = "/shoplist")
     public String shopList() {
+        //转发到店铺列表界面
         return "shop/shoplist";
     }
 
     @RequestMapping(value = "/shopmanagement")
     public String shopManagement() {
+        //转发到店铺管理界面
         return "shop/shopmanagement";
     }
 
     @RequestMapping(value = "/productcategorymanagement")
     public String productCategoryManagement() {
+        //商品类别管理
         return "shop/productcategorymanagement";
     }
 
     @RequestMapping(value = "/productmanagement")
     public String productManagement() {
+        //商品管理
         return "shop/productoperation";
     }
 
 
-
     @RequestMapping(value = "/productoperation")
     public String productOperation() {
+        //商品添加或编辑界面
         return "shop/productoperation";
     }
 
